d3d75979a02c31be57fc5a0be38ade37e5de8b7d
tysonpace777 <32.tpace@gmail.com>
2025-03-05 13:39:31 -0700

    exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,5 +5,29 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1,100);
+
+        int guess = 0;
+        while (guess != number)
+        {
+        Console.WriteLine("What is your guess?");
+        string userInput = Console.ReadLine ();
+        guess = int.Parse(userInput);
+    
+        if (guess > number)
+        {
+            Console.WriteLine("Lower");
+        }
+        else if (guess < number)
+        {
+            Console.WriteLine("Higher");
+        }
+        else if (guess == number)
+        {
+            Console.WriteLine("You guessed it!");
+        }
+        }
     }
 }
