98ee8a234d87f5c780d8d26fb0ec2b4276ea1a48
tysonpace777 <32.tpace@gmail.com>
2025-03-10 13:31:52 -0600

    resume learning activity

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -1,9 +1,24 @@
 using System;
+using System.Reflection;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Resumes Project.");
+
+        Job job1 = new Job();
+        job1._company = "Microsoft";
+        job1._jobTitle = "CEO";
+        job1._startYear = 2019;
+        job1._endYear = 2024;
+
+        job1.Display();
+
+        Resume resume1 = new Resume();
+        resume1._member = "Allison Rose";
+        resume1._jobs.Add(job1);
+
+        resume1.Display();
     }
 }
---
+++ week02/Resumes/Resume.cs
@@ -0,0 +1,18 @@
+using System.Reflection.Metadata.Ecma335;
+
+public class Resume
+{
+    public string _member;
+    public List<Job> _jobs = new List<Job>();
+
+    public void Display()
+    {
+        Console.WriteLine($"Member: {_member}");
+        Console.WriteLine("Jobs:");
+        
+        foreach (Job job in _jobs)
+        {
+            job.Display();
+        }
+    }
+}
