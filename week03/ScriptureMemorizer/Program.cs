8b5300f4b5bc9693390c8b0338c83b0ea7adc1a7
tysonpace777 <32.tpace@gmail.com>
2025-03-19 14:17:03 -0600

    Scripture Memorizer

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -1,9 +1,33 @@
 using System;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
+
+        string scriptureText = "Trust in the Lord with all thine heart and lean not unto thine own understanding; in all thy ways acknowledge him, and he shall direct thy paths.";
+        Reference reference = new Reference("Proverbs", 3, 5, 6);
+        Scripture scripture = new Scripture(reference, scriptureText);
+        
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine(scripture.GetDisplayText());
+
+            Console.WriteLine("Press enter to continue or type 'quit' to finish:");
+            string input = Console.ReadLine();
+
+            if (input == "quit" || scripture.IsCompletelyHidden())
+            {
+                break;
+            }
+            else
+            {
+                scripture.HideRandomWords(3);
+            }
+        }
+
     }
 }
