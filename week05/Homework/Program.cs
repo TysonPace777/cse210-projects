95a5a1a4259b6009ebd269ec19a4821cbe3bc73e
tysonpace777 <32.tpace@gmail.com>
2025-03-30 12:54:09 -0600

    inheritance homework learning activites complete

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -5,5 +5,14 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Homework Project.");
+
+        Assignment assignment1 = new Assignment("Tyson", "Multiplication");
+        Console.WriteLine(assignment1.GetSummary());
+
+        MathAssignment mathassignment2 = new MathAssignment("Tyson", "Multiplication", "7.3", "8-19");
+        Console.WriteLine(mathassignment2.GetHomeworkList());
+
+        WritingAssignment writingassignment3 = new WritingAssignment("Tyson", "European History", "The causes of WW2");
+        Console.WriteLine(writingassignment3.GetWritingInformation());
     }
 }
